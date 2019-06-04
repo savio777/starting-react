e683097ef21ae20dbdb8a70b650cc189f1426a65
savio <saviopf22@gmail.com>
2019-06-04 12:46:19 -0300

    estrutura mais organizada

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+// jsx
+const iae = 'Iae'
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <h1>{iae} React</h1>
+            </div>
+        )
+    }
+}
+
+export default App
