365ef6ac9e0e8c46aecf4e925a989dbc446b9cc8
Savio <saviopf22@gmail.com>
2019-06-05 22:27:16 -0300

    componentes separados

--- src/Tabela.js
+++ src/Tabela.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react'
 
+// componentes separados
+
+const TabelaHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Nomes</th>
+                <th>Idade</th>
+                <th>Altura</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TabelaCorpo = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Sávio</td>
+                <td>21</td>
+                <td>1.745</td>
+            </tr>
+            <tr>
+                <td>Thamara</td>
+                <td>19</td>
+                <td>1.69</td>
+            </tr>
+        </tbody>
+    )
+}
+
 class Tabela extends Component {
+
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Nomes</th>
-                        <th>Idade</th>
-                        <th>Altura</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Sávio</td>
-                        <td>21</td>
-                        <td>1.745</td>
-                    </tr>
-                    <tr>
-                        <td>Thamara</td>
-                        <td>19</td>
-                        <td>1.69</td>
-                    </tr>
-                </tbody>
+                <TabelaHead />
+                <TabelaCorpo />
             </table>
         )
     }
